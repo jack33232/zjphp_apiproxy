dcf9baf3e8d419ff3733da72cc9d386fb6bf39d2
jack33232 <jack33232@gmail.com>
2017-09-29 22:20:47 +0800

    - Rename again

--- Service/Protocol/Http.php
+++ Service/Protocol/Http.php
@@ -14,7 +14,7 @@ use GuzzleHttp\Cookie\CookieJar;
 use GuzzleHttp\Exception\RequestException;
 use Exception;
 
-class HttpProtocal extends Component implements RequestInterface
+class Http extends Component implements RequestInterface
 {
     private $_requestConfig = [];
 
